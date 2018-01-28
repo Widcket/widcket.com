87a9e906e29e0bb38e18afd95fb1cf129c2432e9
Rita Zerrizuela <zeta@widcket.com>
2018-01-28 15:45:06 -0300

    Adapted aside for small portrait size

--- dist/a751d6e348ca1fb2f192a8d814248184.js
+++ dist/a751d6e348ca1fb2f192a8d814248184.js
@@ -132,13 +132,13 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":8}],6:[function(require,module,exports) {
+},{"./bundle-url":8}],5:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":7}],5:[function(require,module,exports) {
+},{"_css_loader":7}],6:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
@@ -150,7 +150,7 @@ module.exports = reloadCSS;
 require('../node_modules/ress/ress.css');
 
 require('../sass/style.scss');
-},{"../node_modules/ress/ress.css":6,"../sass/style.scss":5}],48:[function(require,module,exports) {
+},{"../node_modules/ress/ress.css":5,"../sass/style.scss":6}],14:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -170,7 +170,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '50824' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '57086' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -271,5 +271,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[48,4])
+},{}]},{},[14,4])
 //# sourceMappingURL=/dist/a751d6e348ca1fb2f192a8d814248184.map

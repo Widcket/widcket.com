ccde481b3290c8cd5cc5445b553c06c2fd7d4f07
Rita Zerrizuela <zeta@widcket.com>
2018-01-28 00:47:57 -0300

    Made layout responsive

--- dist/a751d6e348ca1fb2f192a8d814248184.js
+++ dist/a751d6e348ca1fb2f192a8d814248184.js
@@ -150,7 +150,7 @@ module.exports = reloadCSS;
 require('../node_modules/ress/ress.css');
 
 require('../sass/style.scss');
-},{"../node_modules/ress/ress.css":6,"../sass/style.scss":5}],9:[function(require,module,exports) {
+},{"../node_modules/ress/ress.css":6,"../sass/style.scss":5}],32:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -170,7 +170,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '62222' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '50824' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -271,5 +271,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[9,4])
+},{}]},{},[32,4])
 //# sourceMappingURL=/dist/a751d6e348ca1fb2f192a8d814248184.map
